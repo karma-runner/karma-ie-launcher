c75ed348d9e48d4ff6d286a99bd72b8f438a3fe9
Christopher Currie <ccurrie@impresys.com>
2014-01-22 10:47:02 -0800

    feat: support 64-bit IE and isolate environment.
    This patch tries to auto-detect 64-bit IE by checking
    for the appropriate environment variables. It also
    improves reliability of the tests by disabling
    extensions
    
    Closes #1

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
-var IEBrowser = function(baseBrowserDecorator) {
+var fs = require('fs');
+
+var IEBrowser = function(baseBrowserDecorator, args) {
   baseBrowserDecorator(this);
+
+  args = args || {};
+  var flags = args.flags || [];
+
+  this._getOptions = function(url) {
+    return [
+      '-extoff'
+    ].concat(flags, [url]);
+  }
 };
+
+function getInternetExplorerExe() {
+    var suffix = '\\Internet Explorer\\iexplore.exe',
+        prefixes = [process.env['PROGRAMW6432'], process.env['PROGRAMFILES(X86)'], process.env['PROGRAMFILES']],
+        prefix, i;
+
+    for (i = 0; i < prefixes.length; i++) {
+        prefix = prefixes[i];
+        if (prefix && fs.existsSync(prefix + suffix)) {
+            return prefix + suffix;
+        }
+    }
+
+    throw new Error("Internet Explorer not found");
+}
 
 IEBrowser.prototype = {
   name: 'IE',
   DEFAULT_CMD: {
-    win32: process.env.ProgramFiles + '\\Internet Explorer\\iexplore.exe'
+    win32: getInternetExplorerExe()
   },
   ENV_CMD: 'IE_BIN'
 };
